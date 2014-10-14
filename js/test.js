b8f242c71c5350cb899e9c2279c9fb3235bc1326
JoeStratton <Joe.Stratton@asu.edu>
2014-10-14 13:48:06 -0700

    first button attempt

--- js/test.js
+++ js/test.js
@@ -14,3 +14,11 @@
     } catch ($ignore$$) {
     }
 })();
+
+var lines = 0;
+var programmers = 0;
+
+function termclick(number) {
+    lines = lines + number;
+    document.getElementByID("lines").innerHTML = lines;
+};
