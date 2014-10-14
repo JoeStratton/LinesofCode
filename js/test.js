687e2c487b53ae3dc6cabe3ca652512caf0c82ce
JoeStratton <Joe.Stratton@asu.edu>
2014-10-14 14:07:45 -0700

    js update

--- js/test.js
+++ js/test.js
@@ -5,3 +5,20 @@ function termClick(number) {
     lines = lines + number;
     document.getElementByID("lines").innerHTML = lines;
 };
+
+function buyProgrammer() {
+    var progCost = Math.floor(10 * Math.pow(1.1,programmers));
+    if(lines >= progCost){
+        programmers = programmers + 1;
+        lines = lines - progCost;
+        document.getElementByID('programmers').innerHTML = programmers;
+        document.getElementByID('lines').innerHTML = lines;
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,programmers));
+    document.getElementByID('progCost').innerHTML = nextCost;
+};
+
+window.setInterval(function(){
+    //stuff runs in here every second
+    termClick(programmers);
+}, 1000);
